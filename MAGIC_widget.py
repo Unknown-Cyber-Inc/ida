21270dc752983fefaf571b901f78c835d7b2507b
blanchard.lucas <blanchard.lucas@cythereal.com>
2023-04-16 00:41:38 -0500

    Tabulated file info from recieved GET request
    Using IDA Chooser widget object as a table, made a table that prints out
    information about each recieved file (for now the API only returns sha1
    and filetype).
    Needed to temporarily disable register_autoinst_hooks() for now

--- MAGIC_widget.py
+++ MAGIC_widget.py
@@ -28,31 +28,82 @@ PLUGIN_WINDOW_TITLE = 'MAGIC interface'
 
 # -----------------------------------------------------------------------
 """
+This is IDA's chooser class. It is essentially a table with selectable lines
+"""
+class FileListChooser(ida_kernwin.Choose):
+    def __init__(self, title):
+        super().__init__(
+            title,
+            [ ["Sha1", 10 | ida_kernwin.Choose.CHCOL_HEX],
+            ["Filetype",    30 | ida_kernwin.Choose.CHCOL_PLAIN] ],
+            embedded=True)
+        self.items = []
+
+        # .Embedded or .Show is REQUIRED to get the widget pointer
+        # .Show will not work since we set embedded=True
+        self.Embedded()
+        
+    def OnGetSize(self):
+        return len(self.items)
+    
+    def OnGetLine(self, n):
+        return self.items[n]
+    
+    def SetItems(self,items=[]):
+        self.items = items
+
+# -----------------------------------------------------------------------
+"""
 populate_pluginform_with_pyqt_widgets.py code was used to create the base of the plugin
+this is the entire body of the plugin form
 """
 class MAGICPluginFormClass(ida_kernwin.PluginForm):
+
+    """
+    We need both the qw widget to add it to the pyqt layout object
+    and the tw object to actually make modifications to it
+    instead of making PluginForm.objecttw and PluginForm.objectqw
+    I made this class to automatically create the qw from passed tw
+    and store both in the same object
+    """
+    class TWidgetToPyQtWidget:
+        def __init__(self,tw:object):
+            self.tw = tw # tw is IDA python Twidget
+            # qw is PyQt5 QtWidget
+            self.qw = ida_kernwin.PluginForm.TWidgetToPyQtWidget(tw.GetWidget())
+
+
+    def __init__(self):
+        super().__init__()     
+
     def OnCreate(self, form):
         """
         Called when the widget is created
         """
-        # Get parent widget
+        # Convert form to PyQt obj
         self.parent = self.FormToPyQtWidget(form)
+
+        #gather important form information -- consider moving the location of this
         self.sha256 = ida_nalt.retrieve_input_file_sha256().hex()
         self.md5 = ida_nalt.retrieve_input_file_md5().hex()
-        try:
-            self.ctm = cythereal_magic.ApiClient()
-            self.ctmf = cythereal_magic.FilesApi(self.ctm)
-        except:
-            print("Error establishing magic API client.")
-            self.ctm = None
-            self.ctmf = None
+        self.ctm = cythereal_magic.ApiClient()
+        self.ctmfiles = cythereal_magic.FilesApi(self.ctm)
+
+        self.CreateFormObjects()
         self.PopulateForm()
 
-    def PopulateForm(self):
-        # Create layout
-        layout = QtWidgets.QVBoxLayout()
+    def OnClose(self, form):
+        """
+        Called when the widget is closed
+        """
+        pass
 
-        #personalizing widgets
+    def Show(self,title):
+        return super().Show(title)
+
+    def CreateFormObjects(self):
+
+        #personalizing QT widgets
         self.t1 = QtWidgets.QLabel("Hello from <font color=red>PyQt</font>")
         self.t2 = QtWidgets.QLabel("Hello from <font color=blue>IDAPython</font>")
 
@@ -63,48 +114,60 @@ class MAGICPluginFormClass(ida_kernwin.PluginForm):
         self.textbrowser = QtWidgets.QTextEdit()
         self.textbrowser.setReadOnly(True)
 
+        # personalizing T widgets
+        self.filechooser = self.TWidgetToPyQtWidget(FileListChooser("FileListChooser"))
+
+
+    def PopulateForm(self):
+        # Create layout
+        layout = QtWidgets.QVBoxLayout()
+
         #adding widgets
         layout.addWidget(self.t1)
         layout.addWidget(self.t2)
         layout.addWidget(self.pushbutton)
+        layout.addWidget(self.filechooser.qw)
         layout.addWidget(self.textbrowser)
 
         self.parent.setLayout(layout)
 
-    def OnClose(self, form):
-        """
-        Called when the widget is closed
-        """
-        pass
-
     def pushbutton_click(self, form):
         self.textbrowser.clear()
+
         try:
-            ctmr = self.ctmf.list_files()
-            for resource in ctmr.resources:
-                self.textbrowser.append(resource.sha1 + ': ' + resource.filetype)
+            # request file from website
+            ctmr = self.ctmfiles.list_files()
+
+            # add the resources to the chooser object
+            self.filechooser.tw.SetItems([ [ resource.sha1, resource.filetype ] for resource in ctmr.resources ])
+            self.filechooser.tw.Refresh()
+            self.textbrowser.append('Resources gathered successfully.')
         except:
             self.textbrowser.append('No resources could be gathered.')
+            if PLUGIN_DEBUG: 
+                import traceback
+                self.textbrowser.append(traceback.format_exc())
+
 
 # -----------------------------------------------------------------------
-"""
-auto_instantiate_widget_plugin.py code was used to make the plugin window persist on IDA launch
-"""
-def register_autoinst_hooks():
-    """
-    Register hooks that will create the widget when IDA
-    requires it because of the IDB/desktop
-    """
-    class auto_inst_hooks_t(ida_kernwin.UI_Hooks):
-        def create_desktop_widget(self, ttl, cfg):
-            if ttl == PLUGIN_WINDOW_TITLE:
-                MAGICWidgetPage = MAGICPluginFormClass()
-                assert(MAGICWidgetPage.Show(PLUGIN_WINDOW_TITLE))
-                return MAGICWidgetPage.GetWidget()
+# """
+# auto_instantiate_widget_plugin.py code was used to make the plugin window persist on IDA launch
+# """
+# def register_autoinst_hooks():
+#     """
+#     Register hooks that will create the widget when IDA
+#     requires it because of the IDB/desktop
+#     """
+#     class auto_inst_hooks_t(ida_kernwin.UI_Hooks):
+#         def create_desktop_widget(self, ttl, cfg):
+#             if ttl == PLUGIN_WINDOW_TITLE:
+#                 MAGICWidgetPage = MAGICPluginFormClass(PLUGIN_WINDOW_TITLE)
+#                 assert(MAGICWidgetPage.Show(PLUGIN_WINDOW_TITLE))
+#                 return MAGICWidgetPage.GetWidget()
 
-    global auto_inst_hooks
-    auto_inst_hooks = auto_inst_hooks_t()
-    auto_inst_hooks.hook()
+#     global auto_inst_hooks
+#     auto_inst_hooks = auto_inst_hooks_t()
+#     auto_inst_hooks.hook()
 
 # -----------------------------------------------------------------------
 """
@@ -112,7 +175,7 @@ this is the class which manages the plugin entry
 """
 class MAGIC_widget_t(ida_idaapi.plugin_t):
     flags = ida_idaapi.PLUGIN_KEEP 
-    if PLUGIN_DEVELOP: flags = ida_idaapi.PLUGIN_UNL #dev entry - unload plugin from memory when finished
+    if PLUGIN_DEVELOP: flags = ida_idaapi.PLUGIN_UNL #dev entry - unload plugin from memory when widget is closed
     comment = PLUGIN_COMMENT
     help = PLUGIN_HELP
     wanted_name = PLUGIN_NAME
@@ -125,7 +188,7 @@ class MAGIC_widget_t(ida_idaapi.plugin_t):
         #check if our widget is registered with IDA
         #if found, display it
         #if not found, register it
-        register_autoinst_hooks()
+        # register_autoinst_hooks()
 
         print('MAGIC widget -- hotkey is \"%s\"' % PLUGIN_HOTKEY)
         return ida_idaapi.PLUGIN_KEEP
