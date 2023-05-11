fccf4bd94ebba32a08a0bbe4eb29f8756edb36d5
blanchard.lucas <blanchard.lucas@cythereal.com>
2023-05-11 16:17:53 -0500

    custviewer display a bunch of lines

--- MAGIC/MAGIC_sync_scroll.py
+++ MAGIC/MAGIC_sync_scroll.py
@@ -1,9 +1,8 @@
 """
-Main pluginform object at the highest level. 
+Main scroll widget at the highest level. 
 
-This is the scaffolding of the form object which will be displayed to the viewer.
-Contains ida_kernwin.PluginForm and also ida_kernwin.Choose.
-Will likely be broken into components as the insides of the form grow.
+This is the scaffolding of a simplecustviewer_t for the purpose of 
+testing out how certain functions can be synced.
 """
 
 # IDA and UI imports
@@ -20,8 +19,9 @@ PLUGIN_DEBUG = True if os.getenv("PLUGIN_DEBUG") == "True" else False
 class MAGICPluginScrClass(ida_kernwin.simplecustviewer_t):
     def __init__(self,title):
         self.title = title
-        super().__init__()
         
+        super().__init__()
+
         self.Create()
         self.Show()
         # dock this widget on the rightmost side of IDA, ensure this by setting dest_ctrl to an empty string
@@ -31,11 +31,25 @@ class MAGICPluginScrClass(ida_kernwin.simplecustviewer_t):
         # DOESN'T WORK ON TWIDGET
         # self.parent.parent().parent().setSizes([800,1]) 
 
-
-    def Create(self):
+    def Create(self, use_colors=True):
         super().Create(self.title)
+
+        self.use_colors = use_colors
 
         text = __doc__
         for l in text.split("\n"):
             self.AddLine(l)
+
+        for i in range(0, 1000):
+            prefix, bg = ida_lines.COLOR_DEFAULT, None
+            # make every 10th line a bit special
+            if i % 10 == 0:
+                prefix = ida_lines.COLOR_DNAME   # i.e., dark yellow...
+                bg = 0xFFFF00                 # ...on cyan
+            pfx = ida_lines.COLSTR("%3d" % i, ida_lines.SCOLOR_PREFIX)
+            if self.use_colors:
+                self.AddLine("%s: Line %d" % (pfx, i), fgcolor=prefix, bgcolor=bg)
+            else:
+                self.AddLine("%s: Line %d" % (pfx, i))
+
         return True
