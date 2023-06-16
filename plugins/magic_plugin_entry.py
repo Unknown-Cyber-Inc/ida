43fae57d72c810602dcfe8ac4f82fd2bf13c6d97
Lee Hubbard <hubbard.zlee@cythereal.com>
2023-06-16 12:09:58 -0500

    Make initial changes to the plugins

--- plugins/magic_plugin_entry.py
+++ plugins/magic_plugin_entry.py
@@ -4,12 +4,12 @@ Required entrypoint by IDA.
 All IDA python files placed in the plugin folder must contain a unique definition to PLUGIN_ENTRY() which returns the plugin class that tells IDA what to do with it during certain events.
 """
 
-from MAGIC import MAGIC_plugin
+from idamagic import magic
 
 def PLUGIN_ENTRY():
     """
     python plugin entrypoint for IDA. You'll see it in Edit -> Plugins -> PLUGIN_NAME.
-    
+
     @return ida_idaapi.plugin_t: plugin object
     """
-    return MAGIC_plugin()
+    return magic()
